185ef95fd8f176235c302340962a71178089969a
rogerio <rogeriopiatek@gmail.com>
2024-07-10 20:18:40 -0300

    feat: create User entity with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
