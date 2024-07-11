7d012c3ff09b1119349d396eaac2f0753b6d947d
rogerio <rogeriopiatek@gmail.com>
2024-07-10 21:13:49 -0300

    fix: ensure UseGuards decorators are used on the correct place

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GoogleAuthGuard } from './utils/Guards';
 
-@UseGuards(GoogleAuthGuard)
 @Controller('auth')
 export class AuthController {
   @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
   handleLogin() {
     return { msg: 'google authentication' };
   }
 
   // api/auth/google/redirect
   @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
   handleRedirect() {
     return { msg: 'OK' };
   }
