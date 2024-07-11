dd962b939e41d58f74ce2371fc368ddee2b7cdba
rogerio <rogeriopiatek@gmail.com>
2024-07-10 21:23:39 -0300

    feat: add status endpoint to know if the user is authenticated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { GoogleAuthGuard } from './utils/Guards';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,15 @@ export class AuthController {
   @UseGuards(GoogleAuthGuard)
   handleRedirect() {
     return { msg: 'OK' };
+  }
+
+  @Get('status')
+  user(@Req() request: Request) {
+    console.log(request.user);
+    if (request.user) {
+      return { msg: 'Authenticated', user: request.user };
+    } else {
+      return { msg: 'Not Authenticated' };
+    }
   }
 }
